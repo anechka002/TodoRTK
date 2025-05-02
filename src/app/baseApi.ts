e86a3491e1b544469f873d6cdd7db70367c891bd
Anna <blinova.anneta@gmail.com>
2025-05-02 12:47:08 +0200

    errors RTK

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -1,12 +1,13 @@
 import { AUTH_TOKEN } from "@/common/constants.ts"
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import { handleError } from "@/common/utils"
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
   tagTypes: ["Todolist", "Task"],
   baseQuery: async (args, api, extraOptions) => {
 
-    await new Promise(resolve => setTimeout(resolve, 5000))
+    // await new Promise(resolve => setTimeout(resolve, 5000))
 
     const result = await fetchBaseQuery({
       baseUrl: import.meta.env.VITE_BASE_URL,
@@ -15,6 +16,8 @@ export const baseApi = createApi({
         headers.set("Authorization", `Bearer ${localStorage.getItem(AUTH_TOKEN)}`)
       },
     })(args, api, extraOptions)
+
+    handleError(api, result)
     
     return result
   },
---
+++ src/common/utils/isErrorWithMessage.ts
@@ -0,0 +1,11 @@
+/**
+ * Type predicate to narrow an unknown error to an object with a string 'message' property
+ */
+export function isErrorWithMessage(error: unknown): error is { message: string } {
+  return (
+    typeof error === 'object' && // Проверяем, что error – это объект
+    error != null && // Убеждаемся, что это не null
+    'message' in error && // Проверяем, что у объекта есть свойство 'message'
+    typeof (error as any).message === 'string' // Убеждаемся, что это строка
+  )
+}
