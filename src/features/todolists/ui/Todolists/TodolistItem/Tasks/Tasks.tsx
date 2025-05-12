0f80989d0a482be689bb4e13c1e35c128ccc4797
Anna <blinova.anneta@gmail.com>
2025-05-12 11:45:49 +0200

    fix Pagination and Loaders

--- src/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks.tsx
@@ -2,34 +2,17 @@ import List from "@mui/material/List"
 import { TaskItem } from "./TaskItem/TaskItem"
 import { DomainTodolist } from "@/features/todolists/api/todolistsApi.types"
 import { TaskStatus } from "@/common/enum"
-import { useGetTasksQuery } from "@/features/todolists/api/tasksApi"
-import { TasksSkeleton } from "./TasksSkeleton/TasksSkeleton"
-import { useEffect, useState } from "react"
-import { TasksPagination } from "./TasksPagination/TasksPagination"
-import { useAppDispatch } from "@/common/hooks"
-import { setTasksLoading } from "@/app/app-slice"
-import LinearProgress from "@mui/material/LinearProgress"
+import { GetTasksResponseType } from "@/features/todolists/api/tasksApi.types"
 
 type Props = {
   todolist: DomainTodolist
+  tasks: GetTasksResponseType | undefined
+  setPage: (page: number) => void
+  page: number
 }
 
-export const Tasks = ({ todolist }: Props) => {
+export const Tasks = ({ todolist, tasks, setPage, page }: Props) => {
   const { id, filter, entityStatus } = todolist
-
-  const [page, setPage] = useState(1)
-
-  const { data: tasks, isLoading, isFetching } = useGetTasksQuery({ todolistId: id, params: { page } }, {refetchOnFocus: true, pollingInterval: 3000, skipPollingIfUnfocused: true})
-  // refetchOnFocus: true, // для автоматического повторного запроса за данными, когда окно приложения или вкладка браузера возвращаются в фокус.
-  // Polling позволяет автоматически повторять запросы через определённые интервалы времени для поддержания актуальности данных.
-
-  const dispatch = useAppDispatch()
-
-  useEffect(() => {
-    if (!isLoading) {
-      dispatch(setTasksLoading(isFetching))
-    }
-  }, [isFetching, isLoading])
 
   let filteredForTasks = tasks?.items
 
@@ -40,20 +23,20 @@ export const Tasks = ({ todolist }: Props) => {
     filteredForTasks = filteredForTasks?.filter((el) => el.status === TaskStatus.Completed)
   }
 
-  if (isLoading) {
-    return <TasksSkeleton />
-  }
+  const tasksCount = filteredForTasks?.length || 0;
 
   return (
     <div>
-      {/* {isFetching && <LinearProgress />} */}
-      {filteredForTasks?.length === 0 ? (
+      {tasksCount === 0 ? (
         <p>Тасок нет</p>
       ) : (
         <>
           <List>
             {filteredForTasks?.map((task) => (
               <TaskItem
+                page={page}
+                tasksCount={tasksCount}
+                setPage={setPage}
                 disabled={task.entityStatus === "loading"}
                 key={task.id}
                 task={task}
@@ -62,7 +45,6 @@ export const Tasks = ({ todolist }: Props) => {
               />
             ))}
           </List>
-          <TasksPagination page={page} setPage={setPage} totalCount={tasks?.totalCount || 0} />
         </>
       )}
     </div>
